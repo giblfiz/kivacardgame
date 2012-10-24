9414bf1f8ec6f4e6a5825eca6acb59325d69b78e
Harry Pottash <harry@kiva.org>
2012-10-24 16:23:46 -0700

    tweaked CSS, added card type P

--- types/P.php
+++ types/P.php
@@ -1,11 +1,31 @@
-<html>
-<head>
+<div class="asset_card global">
+     <div class="card_name asset <?php echo $card_name_size?>"><? echo $name ?></div>
+     <div class="cost">
+       <div class="big_icon <?echo $cost ?>"><? echo iconify($cost) ?></div>
+     </div>
 
-</head>
+     <div class="effect asset">
+       <div class="floater"></div>
+       <div><? echo iconify($effect) ?></div>
+     </div>
 
-<body>
-<title><? echo $name ?></title>
+     <img class="main_image" src="/kivacardgame/images/<? echo $image ?>" />
+
+     <div class="flavor">
+       <span class="flavor_text"><? echo $flavor ?></span>
+     </div>
+
+     <div class="values">
+       <div class="flip big_icon value <? echo $cash_v ?>">
+	 <? echo iconify('$'.$cash_v) ?>
+       </div>
+       <div class="flip big_icon value <? echo $borrower_v ?>">
+	 <? echo iconify($borrower_v."B") ?>
+       </div>
+       <div class="flip big_icon value <? echo $talent_v ?>">
+	 <? echo iconify($talent_v."T") ?>
+       </div>
+     </div>
 
 
-</body>
-</html>
+</div>
